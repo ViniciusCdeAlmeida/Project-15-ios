e157fc027dad2a967ac1e7c4f689e8bb9d3345e6
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2024-02-03 18:18:39 -0300

    feat: added register user with firebase

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,23 +7,25 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-        let newTitle = "⚡️FlashChat"
-        var charIdx = 0.0
-        for title in newTitle {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIdx, repeats: false, block: { (timer) in
-                self.titleLabel.text?.append(title)
-            })
-            charIdx += 1
-        }
-       
+        titleLabel.text = "⚡️FlashChat"
+//        titleLabel.text = ""
+//        let newTitle = "⚡️FlashChat"
+//        var charIdx = 0.0
+//        for title in newTitle {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIdx, repeats: false, block: { (timer) in
+//                self.titleLabel.text?.append(title)
+//            })
+//            charIdx += 1
+//        }
+
     }
     
 
